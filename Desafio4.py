df2bb37c581c42ba91fb469be3816af9e43e54d6
EderJunior07 <ederjr6@gmail.com>
2020-09-10 21:18:14 -0300

    first commit

---
+++ Desafio4.py
@@ -0,0 +1,15 @@
+numeros = [1,2,3]
+
+
+contador=0
+while(contador<10):
+      if numeros == [1,2,3]:
+            print('é verdade')
+      contador = contador + 1
+      print(numeros)
+
+
+      
+
+    
+
